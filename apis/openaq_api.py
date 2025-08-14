2c5cf93487629f023b4b6a0c924b7c5432615dba
CR Filteau <47042577+Filteau@users.noreply.github.com>
2025-08-14 00:24:59 +0000

    testing out the opanaq api

--- apis/openaq_api.py
+++ apis/openaq_api.py
@@ -7,71 +7,20 @@ Some notes on OpenAQ API usage
 - API reference is under https://docs.openaq.org/api/operations
 '''
 
-
+import os
 from openaq import OpenAQ
-
-client = OpenAQ(api_key="YOUR-OPENAQ-API-KEY")
-client.locations.list(parameters_id=2, limit=1000)
-client.locations.list(coordinates=[136.90610, 35.14942], radius=12000, limit=1000)
-client.close()
+from dotenv import load_dotenv
 
 
-# OpenAQ example response
-# url: https://api.openaq.org/v3/countries/42
-#
-# {
-#   "meta": {
-#     "name": "openaq-api",
-#     "website": "/",
-#     "page": 1,
-#     "limit": 100,
-#     "found": 1
-#   },
-#   "results": [
-#     {
-#       "id": 42,
-#       "code": "KZ",
-#       "name": "Kazakhstan",
-#       "datetimeFirst": "2018-07-27T17:00:00Z",
-#       "datetimeLast": "2024-08-31T21:07:06.731Z",
-#       "parameters": [
-#         {
-#           "id": 1,
-#           "name": "pm10",
-#           "units": "µg/m³",
-#           "displayName": null
-#         },
-#         {
-#           "id": 2,
-#           "name": "pm25",
-#           "units": "µg/m³",
-#           "displayName": null
-#         },
-#         {
-#           "id": 19,
-#           "name": "pm1",
-#           "units": "µg/m³",
-#           "displayName": null
-#         },
-#         {
-#           "id": 98,
-#           "name": "relativehumidity",
-#           "units": "%",
-#           "displayName": null
-#         },
-#         {
-#           "id": 100,
-#           "name": "temperature",
-#           "units": "c",
-#           "displayName": null
-#         },
-#         {
-#           "id": 125,
-#           "name": "um003",
-#           "units": "particles/cm³",
-#           "displayName": null
-#         }
-#       ]
-#     }
-#   ]
-# }
+# load environment vars from .env
+load_dotenv()
+api_key = os.getenv("OPENAQ_API_KEY")
+
+
+with OpenAQ(api_key) as client:
+    location = iter(client.locations.get(2178))
+    client.close()
+
+    for i in location:
+        print(i)
+
